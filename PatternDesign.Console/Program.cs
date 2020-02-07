e2cda5a67add8df896ed3b09701eb80f1c3aa323
Javier García <jagel8@hotmail.com>
2020-02-07 13:19:15 -0600

    First Commit

---
+++ PatternDesign.Console/Program.cs
@@ -0,0 +1,12 @@
+﻿namespace PatternDesign.Console
+{
+    using System;
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
